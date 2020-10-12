aad7a4e30770fbec6994e99b30eb166060cc782d
Prakritee Sharma <praxi99@gmail.comm>
2020-10-12 20:46:26 +0545

    Commit message

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,6 +15,9 @@ class App extends React.Component() {
 
     this.addTrack = this.addTrack.bind(this);    
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -34,19 +37,33 @@ class App extends React.Component() {
     this.setState({playlistTracks: tracks});
   }
 
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+
+  savePlaylist() {
+    let trackURIs = []
+  }
+
+  search(term) {
+    console.log(term)
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          {/* <!-- Add a SearchBar component --> */}
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} 
-                           onAdd={this.addTrack()}
+                           onAdd={this.addTrack}
             />
             <Playlist playlistName={this.state.playlistName} 
                       playlistTracks={this.state.playlistTracks}
                       onRemove={this.removeTrack}
+                      onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist}
                       />
           </div>
         </div>
